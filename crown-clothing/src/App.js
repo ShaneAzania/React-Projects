7f69bde4d3d77df8ef318901ed0de2b03bb6aff2
ShaneAzania <87488883+ShaneAzania@users.noreply.github.com>
2022-10-29 09:45:29 -0400

    installed firebase

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import Nav from "./components/nav/nav";
 import Home from "./routes/home/home";
 import Shop from "./routes/shop/shop";
+import SignIn from "./routes/sign-in/sign-in";
 import { Routes, Route } from "react-router-dom";
 
 
@@ -11,6 +12,7 @@ function App() {
 			<Route path="/" element={<Nav />}>
 				<Route index element={<Home />} />
 				<Route path="shop" element={<Shop />} />
+				<Route path="sign-in" element={<SignIn />} />
 			</Route>
 		</Routes>
 	);
---
+++ crown-clothing/src/routes/sign-in/sign-in.jsx
@@ -0,0 +1,11 @@
+import "./sign-in.scss";
+
+function SignIn() {
+	return (
+		<div className="sign-in">
+			<h1>Sign-In</h1>
+		</div>
+	);
+}
+
+export default SignIn;
