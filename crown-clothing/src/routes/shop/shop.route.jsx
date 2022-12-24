5f14523161c9e0f41dfde23b834411f52b8f992f
ShaneAzania <87488883+ShaneAzania@users.noreply.github.com>
2022-12-24 18:36:44 -0500

    CSS responsiveness and other minor fixes

--- crown-clothing/src/routes/shop/shop.route.jsx
+++ crown-clothing/src/routes/shop/shop.route.jsx
@@ -17,10 +17,10 @@ function Shop() {
 			<div className="categories-container container-fluid">
 				{categories.map((category) => (
 					<div key={category.title} className="row mb-5">
-						<h1 className="">{category.title}</h1>
-						<div className="products-container">
+						<h1 className="text-center pb-3">{category.title}</h1>
+						<div className="products-container row">
 							{category.items.map((product) => (
-								<ProductCard key={product.id} product={product} />
+								<ProductCard className="col-lg-3" key={product.id} product={product} />
 							))}
 						</div>
 					</div>
